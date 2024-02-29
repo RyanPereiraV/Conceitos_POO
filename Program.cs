175a4a9dcbfd9a81bff866a82409b5f3c42a62ea
Ryan Pereira <ryan.pereira117@gmail.com>
2024-02-29 20:13:28 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("11 9999-9999","Nokia1024","948474",8);
+n1.InstalarAplicativo("Telegram");
+n1.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Iphone i1 = new Iphone("11 8888-8888","Iphone1","69848",32);
+i1.InstalarAplicativo("Facebook");
+i1.ReceberLigacao();
